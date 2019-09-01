36e0ccea4cfc7520a9fe6269d9dc4a4c66af5602
MaxWu <maxwu@MaxWus-MacBook-Pro-2.local>
2019-09-01 23:35:51 +0800

    1. dataBinding config

--- app/src/main/java/com/example/musicandvideo/view/MainActivity.java
+++ app/src/main/java/com/example/musicandvideo/view/MainActivity.java
@@ -3,42 +3,37 @@ package com.example.musicandvideo.view;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.musicandvideo.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.musicandvideo.databinding.ActivityMainBinding;
+import com.example.musicandvideo.model.PlayerModel;
+import com.example.musicandvideo.viewmodel.MainActivityModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PlayerModel playerModel;
+    private MainActivityModel mainActivityMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-//        navigationView.setNavigationItemSelectedListener(this);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        playerModel = new PlayerModel();
+        mainActivityMode = new MainActivityModel(this);
+
+        binding.setPlayerModel(playerModel);
+        binding.setMainModel(mainActivityMode);
+
+        mainActivityMode.init(binding,playerModel);
+
     }
 
     @Override
--- app/src/main/java/com/example/musicandvideo/viewmodel/MainActivityModel.java
+++ app/src/main/java/com/example/musicandvideo/viewmodel/MainActivityModel.java
@@ -1,8 +1,66 @@
 package com.example.musicandvideo.viewmodel;
 
 
+import android.view.View;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.musicandvideo.R;
+import com.example.musicandvideo.databinding.ActivityMainBinding;
+import com.example.musicandvideo.model.PlayerModel;
+import com.example.musicandvideo.view.MainActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivityModel extends BaseObservable{
+
+
+    AppBarMainModel appBarMainModel = null;
+    PlayerModel playerModel = null;
+
+    private MainActivity context = null;
+    public MainActivityModel(MainActivity context){
+        this.context = context;
+    }
+
+    @Bindable
+    public AppBarMainModel getAppBarMainModel(){
+        return appBarMainModel;
+    }
+
+    public void setAppBarMainModel(AppBarMainModel appBarMainModel){
+        this.appBarMainModel = appBarMainModel;
+    }
+
+    public void init(ActivityMainBinding binding, PlayerModel playerModel){
+        this.playerModel = playerModel;
+        Toolbar toolbar = binding.appBarMain.toolbar;
+        context.setSupportActionBar(toolbar);
+
+
+        this.appBarMainModel = new AppBarMainModel(this.context);
+        this.appBarMainModel.init(this.playerModel);
+
+        FloatingActionButton fab = binding.appBarMain.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                context, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
 }
